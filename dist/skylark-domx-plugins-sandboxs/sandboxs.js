d96d7e12b93b35c919cac0a2e251f6ceda9a73e6
hudaokeji <github@hudaokeji.com>
2021-06-10 00:54:00 +0900

    init commit

---
+++ dist/skylark-domx-plugins-sandboxs/sandboxs.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-domx-plugins-sandboxs - The skylark sandbox plugins library for dom api extension
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-domx-plugins/skylark-domx-plugins-sandboxs/
+ * @license MIT
+ */
+define(["skylark-langx-ns"],function(n){return n.attach("domx.plugins.popups")});
+//# sourceMappingURL=sourcemaps/sandboxs.js.map
